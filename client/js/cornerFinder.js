af4f87ad5b02b24418469ae770be2e1d21917ab7
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2016-11-08 17:40:08 -0600

    Finish detecting substrokes, also update cornerFinder.js, add some test to index.html

--- client/js/cornerFinder.js
+++ client/js/cornerFinder.js
@@ -13,185 +13,192 @@
       }
     }
     
-    
-	function distance(p1,p2){
-		var x1 = p1.getX();
-		var x2 = p2.getX();
-		var y1 = p1.getY();
-		var y2 = p2.getY();
-		var dx = x1 - x2;
-		var dy = y1 - y2;
-		return Math.sqrt(dx*dx+dy*dy);
-	}
-	function pathDistance(points,a,b){
+    function pathDistance(points,a,b){
 		var d = 0;
-		for (var i = a; i<b;i++){
-			d = d + distance(points[i],points[i+1]);
+		for (var i = a; i< b; i++){
+			d = d + distance(points,i,i+1);
+
 		}
-		return d;
-	} 
-	function isLine(points,a,b){
-		var threshold = 0.95;
-		var Distance = distance(points[a],points[b]);
-		var path = pathDistance(points,a,b);
-		if(Distance/path > threshold) return true;
-		return false;
-	}
-	function determineResampleSpacing(points){
-		var minX = Infinity;
-		var minY = Infinity;
-		var maxX = 0;
-		var maxY = 0;
-		for (var i=0;i<points.length;i++){
-			if(points[i].getX()<minX) minX = points[i].getX();
-			if(points[i].getY()<minY) minY = points[i].getY();
-			if(points[i].getX()>maxX) maxX = points[i].getX();
-			if(points[i].getY()>maxY) maxY = points[i].getY();
-		}
-		var dx = maxX-minX;
-		var dy = maxY-minY;
-		var d = Math.sqrt(dx*dx+dy*dy);
-		return d/6.5
+		return d; 
 	}
 	
-	function resamplePoints(points,S){
-		var resampled = [points[0]]
-		var D = 0;  
-		for (var j = 1; j < points.length; j++) { 
-			var d = distance(points[j-1],points[j]);
-			if(D+d>= S){
-				//var newPoint = jQuery.extend(true, {}, points[0]); 
-				var X = points[j-1].getX()+ (S-D)/d * (points[j].getX()-points[j-1].getX());
-				var Y = points[j-1].getY()+ (S-D)/d * (points[j].getY()-points[j-1].getY());
-				var T = points[j-1].getTime() + ((S-D)/d)*(points[j].getTime() - points[j-1].getTime());
-				var P = new point(X,Y,T); 
-				  
-				resampled.push(P);
-				points.splice(j,0,P);
-				D=0;
-			}
-			else{
-				D = D+d;
-			}
-							
-		}
-		return resampled;
+	function distance(points,a,b){
+		//console.log(points.length,a,b);
+		var x = points[b].x - points[a].x;
+		var y = points[b].y - points[a].y;
+		return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
 	}
-	function getCorners(points){
-		var W = 3;
-		var straw = [];
-		var straw_copy = [];
-		var corners = [0]; 
-		for (var i = W;i< points.length-W;i++){
-			straw[i] = distance(points[i-W],points[i+W]);
-			straw_copy.push(straw[i]);
-		}
-
-		straw_copy.sort();
-		var threshold;
-		if(straw_copy.length%2==0) threshold = 0.95 * (straw_copy[straw_copy.length/2] + straw_copy[straw_copy.length/2-1])/2;
-		else 	threshold = 0.95 * straw_copy[(straw_copy.length-1)/2];
- 		/*
- 		l = [];
- 		for (var i=0;i<W;i++) l.push(0);
- 		for (var i = W;i< points.length-W;i++){
-			l.push(straw[i]-threshold);
-			 
-		} 
- 		console.log(l);
-		*/ 	 
-		var localMin,localMinIndex;
-		
-
-		for(var i=W;i< points.length-W;i++){
-			if(straw[i]<threshold){
-				localMin = Infinity;
-				localMinIndex = i;
-				 
-				while(i< points.length-W && straw[i]<threshold){
-
-					if(straw[i]<localMin){
-						localMin = straw[i];
-						localMinIndex = i;
-					}
-					i++;	
-				}
-				corners.push(i);	
+	
+	function resample(points,S){
+		var D = 0;
+		var resampled = [points[0]];
+		var j = 1;
+		var original = {};
+		original[0] = 0;
+		for (var i = 1; i<points.length; i++){
+			//console.log("reample");
+			var d = distance(points, i-1, i);
+			
+			if (D+d >= S){
+				var x = points[i-1].x + ((S-D)/d)*(points[i].x - points[i-1].x);
+				var y = points[i-1].y + ((S-D)/d)*(points[i].y - points[i-1].y);
+				var p = new point(x,y,0);
+				resampled.push(p);
+				points.splice(i,0,p);
+				original[j] = i;
+				//console.log(j,i,original[j]);
+				j = j+1;
+				D = 0;
+				
+				
+			}
+			else {
+				D = D + d;
 			}
 			
+		}
+		return [resampled,original];
+		
+	}
+	
+	function spacing(points){
+		
+		var maxX = 0, maxY = 0, minX = Infinity, minY = Infinity;
+		for (var i = 0; i< points.length; i++){
+			
+			
+			if ( points[i].x < minX) minX = points[i].x;
+			
+			if ( points[i].y < minY) minY = points[i].y;
 
+			if ( points[i].x > maxX) maxX = points[i].x;
+			
+			if ( points[i].y > maxY) maxY = points[i].y;	
+		}
+		var diagonal = Math.sqrt(Math.pow(maxX-minX,2)+Math.pow(maxY-minY,2));
+		return diagonal/40;
+	}
+	
+	function getCorners(points){
+		//console.log(points.length);
+		var corners = [];
+		corners.push(0);
+		var W = 3;
+		var straws={};
+		var value = [];
+		for (var i = W; i< points.length-W; i++){
+			//console.log("straw",i);
+			straws[i]= distance(points, i-W, i+W);
+			value.push(straws[i]);
 		} 
-
+		//console.log(straws);
+		var t = median(value) * 0.9;
+		//console.log(t);
+		for (var i = W; i< points.length-W; i++){
+			if (straws[i] < t){
+				var min = Infinity;
+				var minIndex = i;
+				while ((i < points.length-W) & (straws[i] < t)){
+					if (straws[i] < min){
+						min = straws[i];
+						minIndex = i;
+					}
+					i = i + 1;
+				}
+				//console.log(i);
+				// don't make the point a corner if it's too close to the previous one
+				if (minIndex-corners[corners.length-1]>5){
+					corners.push(minIndex);		
+				}
+						
+			}		
+		}
 		corners.push(points.length-1);
-		corners = postProcessCorners(points,corners,straw);
-		//console.log(corners);
+		//console.log("corners",corners);
+		corners = postProcessCorners(points,corners, straws);
 		return corners;
 	}
-	function postProcessCorners(points,corners,straw){
-		 
+	
+	// get the medain of an array
+	function median(values) {
+
+		values.sort();
+
+	    var half = Math.floor(values.length/2);
+
+	    if(values.length % 2)
+	        return values[half];
+	    else
+	        return (values[half-1] + values[half]) / 2.0;
+	}
+	
+	function postProcessCorners(points,corners, straws){
 		while(true){
-			 
-			var Continue = true;
-			for (var i=1;i<corners.length;i++){
+			var cont = true; 
+			for (var i = 1; i< corners.length;i++){
 				var c1 = corners[i-1];
 				var c2 = corners[i];
-				//console.log(points.length.toString()+' '+c1.toString()+' '+c2.toString());
-				
-				if(!isLine(points,c1,c2)) {
-					
-					var newCorner = halfwayCorner(straw,c1,c2);
-					//console.log(points.length.toString()+' '+c1.toString()+' '+c2.toString()+ ' '+ newCorner.toString());
-					if(newCorner!=-1){
- 
-						corners.splice(i,0,newCorner);
-						Continue=false;	
-					}
-					
-					//i+=1;
-					//console.log(c1.toString()+' '+newCorner.toString()+' '+c2.toString());
-					
-					
-					
-				}
-				//console.log(i.toString());
-			}
-			if(Continue==true) break; 
-
-
-		}
- 
-		
-		for (var i=1;i<corners.length-1;i++){
-			var c1 = corners[i-1];
-			var c2 = corners[i+1];
-			if(isLine(points,c1,c2)){
-				corners.splice(i,1);
-				i=i-1;
-			}
-		}
-		
-		return corners;
+				if (!isLine(points,c1,c2)){
+					if ((c1>=3) & (c2<= points.length-3)){
+						var newCorner = halfWayCorner(straws,c1,c2);
+						//console.log("newCorner",newCorner,"c2",c2);
+						if (newCorner != c1 & newCorner!=c2 ) {
+							corners.splice(i,0,newCorner);
+							cont = false;
+						}
 						
+					//	console.log("newCorner",newCorner);
+					//  console.log(corners);
+					}			
+				}
+			}
+			if (cont == true) break;
+		}
+			for (var i =1; i<corners.length-1; i++){
+				var c1 = corners[i-1];
+				var c2 = corners[i+1];
+				if (isLine(points,c1,c2)){
+					corners.splice(i,1);
+					//console.log(i);
+					//console.log(corners);
+					i = i -1;
+				}
+			}
+			return corners;
+		
+		
+	}
+	
+	function isLine(points, a, b){
+		//console.log("isLine",a,b);
+		var threshold = 0.9;
+		//var x = distance(points,a,b);
+		//console.log(x);
+		var x = distance(points,a,b);
+		var y = pathDistance(points,a,b);  
+		if (x/y > threshold){
+			return true;
+		}
+		else{
+			//console.log(x,y);
+			//console.log("false");
+			return false;
+		}
+			
+	}
+	
+	function halfWayCorner(straws,a,b){
+		var quarter = Math.floor((b-a)/4);
+		//console.log("quarter",quarter);
+		var min = Infinity;
+		for (var i = a+quarter; i<= b-quarter;i++){
+			if (straws[i] < min){
+				min = straws[i];
+				var minIndex = i;
+			}
+		}
+		//console.log("insert",a,b,minIndex);
+		return minIndex;
 	}
 
-	function halfwayCorner(straw,a,b){
-		var quarter = 	Math.floor((b-a)/4+0.0000001);
-		 
-		
-		var minVal = Infinity;
-		var minIndex; 
-
- 
-
-		for (var i=a+quarter;i<=b-quarter;i++){
-			if(straw[i]!=undefined && straw[i]<minVal){
-				minVal = straw[i];
-				minIndex = i; 
-			}
-		} 
-		if(minIndex==undefined || minIndex ==a || minIndex==b) {
-			return -1;
-
-		}
-		return minIndex;
-	} 
+	
